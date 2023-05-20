b44fc6a2397009b9201a88c5ca82c454dfc49b47
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-20 23:13:53 +0530

    make button flex

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 export const buttonVariants = cva(
-  "px-6 py-2 rounded-md transition-all duration-200 font-bold",
+  "px-6 py-2 rounded-md transition-all duration-200 font-bold flex items-center",
   {
     variants: {
       variant: {
