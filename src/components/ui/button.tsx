e18178f472a4a8e027b8689d5f54c182123b6aa8
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-20 21:02:25 +0530

    fix style of outlined button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,12 +1,14 @@
 import { cva } from "class-variance-authority";
 
 export const buttonVariants = cva(
-  "px-6 py-2 rounded-md transition-transform duration-200 hover:shadow-md hover:-translate-y-1 font-bold",
+  "px-6 py-2 rounded-md transition-all duration-200 font-bold",
   {
     variants: {
       variant: {
-        filled: "bg-primary text-secondary hover:bg-primary/90",
-        outlined: "border-2 border-secondary",
+        filled:
+          "bg-primary text-secondary hover:bg-primary/90 hover:shadow-md hover:-translate-y-1",
+        outlined:
+          "border-2 border-secondary hover:bg-secondary hover:text-gray-100",
         link: "hover:border-b-2",
       },
       fillWidth: {
