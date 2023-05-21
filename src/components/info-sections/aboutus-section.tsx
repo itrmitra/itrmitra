ddeb25c89a0dbf116a99cb82ae165669358e6052
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-21 18:12:53 +0530

    fix(ui): make mobile responsive

--- src/components/info-sections/aboutus-section.tsx
+++ src/components/info-sections/aboutus-section.tsx
@@ -1,39 +1,40 @@
-import { CheckCircle, ArrowRight } from "lucide-react";
-import { buttonVariants } from "../ui/button";
-import { cn } from "../../lib/utils";
+import { CheckCircle, ArrowRight } from "lucide-react"
+import { buttonVariants } from "../ui/button"
+import { cn } from "../../lib/utils"
 
 export default function AboutUsSection() {
-  return (
-    <div className=" grid grid-cols-8 min-h-screen bg-[#ECF5CC]">
-      <div className="col-span-4 my-auto">
-        <img src="/team-working.svg" />
-      </div>
-      <div className="col-span-3 my-auto">
-        <h2 className="font-semibold  text-lg text-secondary">ABOUT US</h2>
-        <h1 className="font-extrabold text-6xl tracking-tighter leading-tight mb-8">
-          We are here to make your tax filing process more easier!
-        </h1>
-        <ul className="text-lg mb-8 space-y-4">
-          <li className="flex items-center gap-2">
-            <CheckCircle />
-            Convenient Processing
-          </li>
-          <li className="flex items-center gap-2">
-            <CheckCircle />
-            Convenient Processing
-          </li>
-          <li className="flex items-center gap-2">
-            <CheckCircle />
-            Convenient Processing
-          </li>
-        </ul>
-        <div className="flex">
-          <button className={cn(buttonVariants(), "gap-2")}>
-            Know More <ArrowRight size={20} />
-          </button>
+    return (
+        <div className="py-8 md:py-0 md:min-h-screen flex flex-col  md:flex-row items-center bg-[#ECF5CC]">
+            <div className="flex-1 mb-8 md:mb-0">
+                <img src="/team-working.svg" className="h-52 md:h-auto" />
+            </div>
+            <div className="flex-1">
+                <div className="w-3/4 flex flex-col mx-auto text-center md:text-start items-center md:items-start">
+                    <h2 className="font-semibold text-sm lg:text-lg text-secondary mb-4">
+                        ABOUT US
+                    </h2>
+                    <h1 className="font-extrabold text-4xl md:text-5xl lg:text-6xl tracking-tighter leading-tight mb-6">
+                        We are here to make your tax filing process easier!
+                    </h1>
+                    <ul className="text-sm md:text-base lg:text-lg mb-8 space-y-4">
+                        <li className="flex items-center gap-2">
+                            <CheckCircle />
+                            Quality and on-time services.
+                        </li>
+                        <li className="flex items-center gap-2">
+                            <CheckCircle />
+                            Expert team members with industry experience.
+                        </li>
+                        <li className="flex items-center gap-2">
+                            <CheckCircle />
+                            Secure processing of your documents.
+                        </li>
+                    </ul>
+                    <button className={cn(buttonVariants(), "gap-2")}>
+                        Know More <ArrowRight size={20} />
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="col-span-1"></div>
-    </div>
-  );
+    )
 }
--- src/components/info-sections/landing-section.tsx
+++ src/components/info-sections/landing-section.tsx
@@ -1,23 +1,23 @@
-import { buttonVariants } from "../ui/button";
+import { cn } from "../../lib/utils"
+import { buttonVariants } from "../ui/button"
 
 export default function LandingSection() {
-  return (
-    <div className="min-h-screen grid grid-cols-8">
-      <div className="col-span-1"></div>
-      <div className="col-span-3 my-auto">
-        <h1 className="font-extrabold text-6xl tracking-tighter leading-tight mb-4">
-          Best Digital Agency Whole Online Today
-        </h1>
-        <p className="text-lg mb-8">
-          We help clients with their taxations and whole tax filing process.
-        </p>
-        <div className="flex">
-          <button className={buttonVariants()}>Write us a query</button>
+    return (
+        <div className="py-8 md:py-0 md:min-h-screen flex flex-col-reverse md:flex-row items-center">
+            <div className="flex-1">
+                <div className="w-3/4 flex flex-col mx-auto text-center md:text-start items-center md:items-start">
+                    <h1 className="font-extrabold text-4xl md:text-5xl lg:text-6xl tracking-tighter leading-tight mb-4">
+                        Simplify Your Tax Filing Experience with Expert Guidance
+                    </h1>
+                    <p className="text-sm md:text-base lg:text-lg mb-8">
+                        Maximize Returns, Minimize Stress: Your Trusted Tax Filing Partner
+                    </p>
+                    <button className={cn(buttonVariants())}>Write us a query</button>
+                </div>
+            </div>
+            <div className="flex-1 mb-8 md:mb-0">
+                <img src="/personal-finance.svg" className="h-52 md:h-auto" />
+            </div>
         </div>
-      </div>
-      <div className="col-span-4 my-auto">
-        <img src="/personal-finance.svg" />
-      </div>
-    </div>
-  );
+    )
 }
