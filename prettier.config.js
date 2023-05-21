dd3c78eaa7964ac208d3358162d6be4849c897e9
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-21 18:14:31 +0530

    add prettierconfig

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tailwindConfig: "./tailwind.config.js",
+    // plugins: [require("prettier-plugin-tailwindcss")],
+    tabWidth: 4,
+    semi: false,
+    singleQuote: false,
+    printWidth: 100,
+}
