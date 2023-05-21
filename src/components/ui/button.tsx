a5319447942a5658a7d6177d9140d5ba2ff089e4
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-21 18:13:11 +0530

    add icon support and icon size to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,22 +1,26 @@
-import { cva } from "class-variance-authority";
+import { cva } from "class-variance-authority"
 
 export const buttonVariants = cva(
-  "px-6 py-2 rounded-md transition-all duration-200 font-bold flex items-center",
-  {
-    variants: {
-      variant: {
-        filled:
-          "bg-primary text-secondary hover:bg-primary/90 hover:shadow-md hover:-translate-y-1",
-        outlined:
-          "border-2 border-secondary hover:bg-secondary hover:text-gray-100",
-        link: "hover:border-b-2",
-      },
-      fillWidth: {
-        true: "w-full",
-      },
-    },
-    defaultVariants: {
-      variant: "filled",
-    },
-  }
-);
+    "rounded-md transition-all duration-200 font-bold flex items-center",
+    {
+        variants: {
+            variant: {
+                filled: "bg-primary text-secondary hover:bg-primary/90 hover:shadow-md hover:-translate-y-1",
+                outlined: "border-2 border-secondary hover:bg-secondary hover:text-gray-100",
+                link: "hover:border-b-2",
+                icon: "h-12 aspect-square rounded-full hover:bg-secondary hover:text-gray-100",
+            },
+            fillWidth: {
+                true: "w-full",
+            },
+            size: {
+                default: "px-4 py-2 md:px-6 md:py-2 text-sm md:text-base w-fit",
+                icon: "p-0 grid place-items-center",
+            },
+        },
+        defaultVariants: {
+            variant: "filled",
+            size: "default",
+        },
+    }
+)
