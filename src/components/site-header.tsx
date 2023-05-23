c2953cde9b743fdc0c9f5c6eb26990b1137e5a94
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-23 23:14:53 +0530

    fix: dropdown menu

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
-import { cn } from "../lib/utils"
 import { MainNav } from "./main-nav"
 import { buttonVariants } from "./ui/button"
 
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
 
 import { Menu } from "lucide-react"
+import WhatsappButton from "./whatsapp_button"
 
 const items = [
     {
@@ -28,14 +28,13 @@ export function SiteHeader() {
     return (
         <header className="sticky top-0 z-50 border-b backdrop-blur-xl">
             <div className="px-8 sm:px-16 lg:px-0 lg:container h-20 flex items-center justify-between">
-                <h1 className="font-extrabold tracking-tighter text-2xl">ITR Mitra</h1>
-                <MainNav items={items} currentHref={router.pathname} />
-                <Link
-                    href="/contact"
-                    className={cn(buttonVariants({ variant: "outlined" }), "hidden md:flex")}
-                >
-                    Contact Us
+                <Link href="/">
+                    <h1 className="font-extrabold tracking-tighter text-2xl">ITR Mitra</h1>
                 </Link>
+                <MainNav items={items} currentHref={router.pathname} />
+                <div className="hidden md:block">
+                    <WhatsappButton />
+                </div>
                 <div className="md:hidden">
                     <DropdownMenu.Root>
                         <DropdownMenu.Trigger asChild>
@@ -45,7 +44,7 @@ export function SiteHeader() {
                         </DropdownMenu.Trigger>
                         <DropdownMenu.Portal>
                             <DropdownMenu.Content
-                                className="min-w-[8rem] z-40 relative rounded-md shadow-md py-1 overflow-hidden bg-gray-100"
+                                className="min-w-[8rem] z-50  backdrop-blur-xl bg-gray-100/50 relative rounded-md shadow-md py-1 overflow-hidden text-sm sm:text-base"
                                 side="top"
                                 sideOffset={4}
                             >
@@ -54,18 +53,11 @@ export function SiteHeader() {
                                         <DropdownMenu.Item
                                             key={item.href}
                                             asChild
-                                            className="relative select-none outline-none transition-colors flex p-2 focus:bg-secondary focus:text-gray-100"
+                                            className="relative select-none outline-none transition-colors flex p-2 focus:bg-brand focus:text-white"
                                         >
                                             <Link href={item.href}>{item.title}</Link>
                                         </DropdownMenu.Item>
                                     ))}
-                                    <DropdownMenu.Separator />
-                                    <DropdownMenu.Item
-                                        asChild
-                                        className="relative gap-1 items-center select-none outline-none transition-colors flex p-2 focus:bg-secondary focus:text-gray-100"
-                                    >
-                                        <Link href="/contact">Contact Us</Link>
-                                    </DropdownMenu.Item>
                                 </>
                             </DropdownMenu.Content>
                         </DropdownMenu.Portal>
