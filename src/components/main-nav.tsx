7b4be17d2e71ace223b81d70fac2a8b00b355682
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-23 23:35:40 +0530

    change selected tab text color

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,32 +1,30 @@
-import Link from "next/link";
-import { cn } from "../lib/utils";
+import Link from "next/link"
+import { cn } from "../lib/utils"
 
 export interface NavItem {
-  title: string;
-  href: string;
+    title: string
+    href: string
 }
 
 interface MainNavProps {
-  items: NavItem[];
-  currentHref: string;
+    items: NavItem[]
+    currentHref: string
 }
 
 export function MainNav({ items, currentHref }: MainNavProps) {
-  return (
-    <nav className="hidden md:flex items-center justify-evenly space-x-12 px-8 text-sm">
-      <>
-        {items.map((item) => (
-          <Link
-            key={item.href}
-            href={item.href}
-            className={cn(
-              currentHref == item.href && "font-bold border-b-2 border-b-black"
-            )}
-          >
-            {item.title}
-          </Link>
-        ))}
-      </>
-    </nav>
-  );
+    return (
+        <nav className="hidden md:flex items-center justify-evenly space-x-12 px-8 text-sm">
+            <>
+                {items.map((item) => (
+                    <Link
+                        key={item.href}
+                        href={item.href}
+                        className={cn(currentHref == item.href && "font-bold text-brand")}
+                    >
+                        {item.title}
+                    </Link>
+                ))}
+            </>
+        </nav>
+    )
 }
