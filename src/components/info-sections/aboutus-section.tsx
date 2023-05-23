39044ff771585455f1b8a5f8798799e96b010603
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-23 23:12:50 +0530

    change illustration

--- src/components/info-sections/aboutus-section.tsx
+++ src/components/info-sections/aboutus-section.tsx
@@ -6,13 +6,11 @@ export default function AboutUsSection() {
     return (
         <div className="py-8 md:py-0 md:min-h-screen flex flex-col  md:flex-row items-center">
             <div className="flex-1 mb-8 md:mb-0">
-                <img src="/team-working.svg" className="h-52 md:h-auto" />
+                <img src="/meet-the-team.svg" className="h-52 md:h-auto" />
             </div>
             <div className="flex-1">
                 <div className="w-3/4 flex flex-col mx-auto items-center md:items-start">
-                    <h2 className="font-semibold text-sm lg:text-lg text-secondary mb-4 text-center md:text-start">
-                        ABOUT US
-                    </h2>
+                    <h2 className="text-sm lg:text-lg mb-4 text-center md:text-start">ABOUT US</h2>
                     <h1 className="font-extrabold text-4xl md:text-5xl lg:text-6xl tracking-tighter leading-tight mb-6 text-center md:text-start">
                         We are here to make your tax filing process easier!
                     </h1>
