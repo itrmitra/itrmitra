0e8b1f9a1e48fabab84d22857e61989d63811f34
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-23 18:53:50 +0530

    add success indication message

--- src/components/info-sections/form-filing-section.tsx
+++ src/components/info-sections/form-filing-section.tsx
@@ -19,24 +19,32 @@ export default function FormFilingSection() {
         type: "FILE ITR",
     })
     const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [success, setSuccess] = useState(false)
 
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         setError((prev) => "")
+        setSuccess((prev) => false)
+
         let re = /^\d{10}$/
         if (!re.test(formState.phone)) {
             setError((prev) => "Phone Number is not valid")
             return
         }
+
         let emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
         if (!emailReg.test(formState.email)) {
             setError((prev) => "Email is not valid")
             return
         }
 
-        sendEmail(formState)
+        setLoading(true)
 
-        // send email using formstate
+        const res = await sendEmail(formState)
+        if (!res) setError("Could not send email. Please Try Again Later.")
+        else setSuccess(true)
+        setLoading(false)
     }
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -50,11 +58,14 @@ export default function FormFilingSection() {
             <div className="flex-1">
                 <div className="w-3/4 flex flex-col mx-auto text-center md:text-start items-center md:items-start"></div>
             </div>
-            <div className="flex-1 mb-8 md:mb-0 z-0">
+            <div className="flex-1 mb-8 md:mb-0 z-0 px-4 sm:px-0">
                 <h2 className="font-extrabold text-3xl tracking-tighter mb-4 text-secondary">
                     Connect me with ITR Mitra
                 </h2>
-                <form onSubmit={onSubmit} className="flex flex-col gap-4 md:w-3/4">
+                <form
+                    onSubmit={onSubmit}
+                    className="flex flex-col gap-4 md:w-3/4 text-sm md:text-base"
+                >
                     <div className="flex flex-col gap-2">
                         <label htmlFor="fullname" className="font-bold">
                             Name
@@ -110,29 +121,41 @@ export default function FormFilingSection() {
                     <div className="flex gap-4 font-bold">
                         <div className="flex gap-2">
                             <input
+                                required
                                 id="fileitr"
                                 type="radio"
                                 name="type"
                                 value="FILE ITR"
                                 onChange={onChange}
                             />
-                            <label htmlFor="fileitr">I want to file ITR</label>
+                            <label htmlFor="fileitr" className="cursor-pointer">
+                                I want to file ITR
+                            </label>
                         </div>
                         <div className="flex gap-2">
                             <input
+                                required
                                 id="consultancy"
                                 type="radio"
                                 name="type"
                                 value="CONSULTANCY"
                                 onChange={onChange}
+                                className=""
                             />
-                            <label htmlFor="consultancy">I need Consultancy</label>
+                            <label htmlFor="consultancy" className="cursor-pointer">
+                                I need Consultancy
+                            </label>
                         </div>
                     </div>
 
-                    <button type="submit" className={buttonVariants()}>
-                        Get Started Now
-                    </button>
+                    <div className="flex flex-col md:flex-row gap-4 items-start md:items-center">
+                        <button type="submit" className={buttonVariants()} disabled={loading}>
+                            {loading ? "Loading..." : "Get Started"}
+                        </button>
+                        <span className="text-green-600 font-bold">
+                            {success && "Our experts will contact you after some time."}
+                        </span>
+                    </div>
                 </form>
             </div>
         </div>
