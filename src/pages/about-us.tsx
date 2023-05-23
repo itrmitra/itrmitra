7936d9a0c48428aee60cd424cafe6741112a24a9
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-23 23:23:47 +0530

    add message to indicate development

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,8 +1,13 @@
 export default function AboutUs() {
-  return (
-    <>
-      <title>ITR Mitra | About Us</title>
-      About Us
-    </>
-  );
+    return (
+        <>
+            <title>ITR Mitra | About Us</title>
+            <div className="min-h-screen flex flex-col justify-center items-center">
+                <h1 className="text-4xl md:text-4xl lg:text-6xl font-extrabold tracking-tighter">
+                    In Development...
+                </h1>
+                <p className="text-2xl mt-4">Stay Tuned</p>
+            </div>
+        </>
+    )
 }
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,8 +1,13 @@
 export default function Services() {
-  return (
-    <>
-      <title>ITR Mitra | Services</title>
-      Services
-    </>
-  );
+    return (
+        <>
+            <title>ITR Mitra | Services</title>
+            <div className="min-h-screen flex flex-col justify-center items-center">
+                <h1 className="text-4xl md:text-4xl lg:text-6xl font-extrabold tracking-tighter">
+                    In Development...
+                </h1>
+                <p className="text-2xl mt-4">Stay Tuned</p>
+            </div>
+        </>
+    )
 }
