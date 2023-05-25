055bb7300f2a3558d811a6a280faedc471308025
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-25 20:38:23 +0530

    add accordion animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,12 +30,22 @@ module.exports = {
                     from: { opacity: 0, transform: "translateX(-2px)" },
                     to: { opacity: 1, transform: "translateX(0)" },
                 },
+                "accordion-down": {
+                    from: { height: 0 },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: 0 },
+                },
             },
             animation: {
                 slideDownAndFade: "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
                 slideLeftAndFade: "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
                 slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
                 slideRightAndFade: "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             },
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -45,12 +55,14 @@ module.exports = {
             backgroundColor: {
                 brand: "var(--brand-color)",
                 "light-brand": "var(--light-brand-color)",
+                "dark-gray": "var(--dark-gray)",
             },
             borderColor: {
                 brand: "var(--brand-color)",
+                "dark-gray": "var(--dark-gray)",
             },
             textColor: {
-                primary: "var(--text-color)",
+                primary: "var(--dark-gray)",
                 brand: "var(--brand-color)",
             },
         },
