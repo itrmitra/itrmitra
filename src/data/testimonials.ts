a1baec3a00085d90db516bf2ca675ed86103f2ac
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-25 18:51:50 +0530

    update image urls

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -1,7 +1,7 @@
 export const testimonials = [
     {
         name: "Sanit Jain",
-        text: "Very satisfied with taxbuddy service. Mr. Krishna very professional in his behavior and filed my return on the same day without any hassle. I got all my queries answered before filing the returns and had very quick response from him.I really liked the service and excellent work. Thanks ITR Mitra",
+        text: "Very satisfied with ITR Mitra service. Mr. Jitendra very professional in his behavior and filed my return on the same day without any hassle. I got all my queries answered before filing the returns and had very quick response from him.I really liked the service and excellent work. Thanks ITR Mitra",
         numStars: 4,
         imageUrl: "/sanit-jain.jpg",
     },
@@ -15,13 +15,12 @@ export const testimonials = [
         name: "Sameer Tiwari",
         text: "I have been taking tax consulting service from Jitendra for past 3 plus years. He has given me excellent guidance and always prompt on filing my return taxes. I would recommend his service to my friends too. Thanks for your help and guidance thus far. Keep up the good work.",
         numStars: 5,
-        imageUrl: "/guruswami-hiremath.jpg",
+        imageUrl: "/sameer-tiwari.jpg",
     },
     {
         name: "Nilam Dubey",
         text: "It was very Hassle free communication and service from Jitendra he was very patient during the all service which he provided to me in last year. THANK YOU",
         numStars: 5,
-        imageUrl: "rahul-sharma.jpg",
     },
     {
         name: "Shrutika Wani",
@@ -33,6 +32,6 @@ export const testimonials = [
         name: "Brijesh Nair",
         text: "I'm filing my ITR from Jitendra from last 3 years, he is very professional in his work, excellent command over taxation and give me best advice as possible, guided me soo much with my taxes. Now I cam focus on my business and need not to worry about tax and accounts part, Jitendra take care of everything. \nThank you Jitendra, keep it up ITR Mitra.",
         numStars: 4,
-        imageUrl: "/rohit-bhatia.jpg",
+        imageUrl: "/brijesh-nair.jpg",
     },
 ]
