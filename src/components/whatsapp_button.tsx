84658bbd34d6b9fe95828308641e3eeaa008dfd9
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-25 20:35:10 +0530

    update ui of whatsapp button

--- src/components/whatsapp_button.tsx
+++ src/components/whatsapp_button.tsx
@@ -7,10 +7,14 @@ const WhatsappButton = ({ className }: { className?: string }) => {
         <a
             href={`https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP_NUMBER}`}
             target="_blank"
-            className={cn(buttonVariants({ variant: "filled" }), "gap-2", className)}
+            className={cn(
+                buttonVariants({ variant: "filled" }),
+                "gap-2 bg-white text-primary",
+                className
+            )}
         >
             Connect on
-            <img src="/whatsapp.svg" className="h-6 md:h-6" />
+            <img src="/whatsapp.svg" className="h-7 w-7" />
         </a>
     )
 }
