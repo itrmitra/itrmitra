cec66f988691e850e7b3483e6f3ee953fdcaeaad
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-26 22:09:17 +0530

    add notices and services data

---
+++ src/data/notices.ts
@@ -0,0 +1,34 @@
+export const notices = [
+    {
+        title: "Share notice with ease",
+        subtitle: "Share notice through mail or whatsapp us.",
+    },
+    {
+        title: "Clear Explanation",
+        subtitle:
+            "We'll explain the notice in simple terms, making sure you fully comprehend its requirements and implications",
+    },
+    {
+        title: "Personalized Guidance",
+        subtitle:
+            "Receive tailored guidance on how to respond to the notice effectively and meet the necessary obligations",
+    },
+    {
+        title: "Document Assistance",
+        subtitle:
+            "We'll assist you in gathering and organizing the required documents to address the notice accurately",
+    },
+    {
+        title: "Expert Review",
+        subtitle: "Thorough review and expert advice on drafting a suitable response",
+    },
+    {
+        title: "Free of cost Revised Returns Filing",
+        subtitle: "Assistance in filing revised returns for Section 139(9) or 143(1) notices",
+    },
+    {
+        title: "Peace of Mind",
+        subtitle:
+            "Trust in our expertise and dedicated support to alleviate any stress or concerns related to income tax notices",
+    },
+]
