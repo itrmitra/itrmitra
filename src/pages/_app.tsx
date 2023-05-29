4842692ae01f0cd95cf06840f61b946411ab491e
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-05-29 16:04:16 +0530

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import RootLayout from "../components/root-layout";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import RootLayout from "../components/root-layout"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
-  );
+    return (
+        <>
+            <RootLayout>
+                <Component {...pageProps} />
+            </RootLayout>
+            <Analytics />
+        </>
+    )
 }
