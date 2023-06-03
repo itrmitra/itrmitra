004044e5c623a96c1267b01c2d67a3547d36dd75
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-03 19:08:22 +0530

    add about us tab ui

---
+++ src/data/aboutus.ts
@@ -0,0 +1,32 @@
+export const aboutus = [
+    {
+        title: "Expertise That Empowers",
+        content:
+            "Our seasoned professionals have mastered the intricacies of taxation, ensuring accurate and reliable solutions tailored to your needs.",
+        imageUrl: "/empower.svg",
+    },
+    {
+        title: "Seamless and Swift",
+        content:
+            "Say goodbye to tax stress! Our user-friendly online platform ensures a smooth experience from start to finish.",
+        imageUrl: "/mobile-user.svg",
+    },
+    {
+        title: "Personalized Guidance",
+        content:
+            "We understand that your tax situation is unique. Count on us to provide tailored advice and proactive tax planning strategies.",
+        imageUrl: "/guidance.svg",
+    },
+    {
+        title: "Trustworthy Partnership",
+        content:
+            "Trust is our foundation. Rest assured that your confidential information is secure with us, and our integrity is unwavering.",
+        imageUrl: "/partnership.svg",
+    },
+    {
+        title: "Unlock Your Financial Potential",
+        content:
+            "Let us help you navigate the tax landscape, optimizing your tax savings and empowering your financial goals.",
+        imageUrl: "/growth.svg",
+    },
+]
--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,13 +1,69 @@
+import { aboutus } from "../data/aboutus"
+
 export default function AboutUs() {
     return (
         <>
             <title>ITR Mitra | About Us</title>
-            <div className="flex min-h-screen flex-col items-center justify-center">
-                <h1 className="text-4xl font-extrabold tracking-tighter md:text-4xl lg:text-6xl">
-                    In Development...
+            <meta
+                name="description"
+                content="ITR Mitra is a initiative of experts in Income Tax Filing processess."
+            />
+            <div className="flex flex-col-reverse items-center py-12 md:min-h-screen md:flex-row md:py-0">
+                <div className="flex-1">
+                    <div className="mx-auto flex w-3/4 flex-col items-center text-center md:items-start md:text-start">
+                        <h1 className="mb-4 text-4xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
+                            Welcome to ITR Mitra
+                        </h1>
+                        <p className="mb-8 text-sm md:text-base lg:text-lg">
+                            Your Tax Solution Companion! With 8 years of experience in the taxation
+                            realm, we're thrilled to launch our online portal, bringing our
+                            expertise to your fingertips. Backed by a team of competent tax
+                            professionals and Chartered Accountants, we're here to provide you with
+                            timely assistance whenever you need it.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex-1">
+                    <img src="/about-us.svg" className="h-52 md:h-auto" alt="About Us" />
+                </div>
+            </div>
+            <div className="flex flex-col bg-light-brand px-8 py-12 md:px-12">
+                <h1 className=" mx-auto mb-4 text-4xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
+                    Why choose ITR Mitra?
                 </h1>
-                <p className="mt-4 text-2xl">Stay Tuned</p>
+                <div className="mt-8 flex flex-col gap-6 md:flex-row">
+                    {aboutus.slice(0, -2).map((item) => (
+                        <FeatureCard {...item} />
+                    ))}
+                </div>
+
+                <div className="mt-6 flex flex-col items-stretch justify-center gap-6 md:flex-row">
+                    {aboutus.slice(-2).map((item) => (
+                        <FeatureCard {...item} />
+                    ))}
+                </div>
             </div>
         </>
+    )
+}
+
+function FeatureCard({
+    title,
+    imageUrl,
+    content,
+}: {
+    title: string
+    imageUrl: string
+    content: string
+}) {
+    return (
+        <div
+            key={title}
+            className="basis-1/3 rounded-xl bg-white p-8 text-center shadow-lg transition-colors"
+        >
+            {imageUrl && <img src={imageUrl} className="mx-auto mb-8 h-20" />}
+            <h3 className="mb-2 text-lg font-semibold md:text-xl">{title}</h3>
+            <p className="text-sm md:text-base">{content}</p>
+        </div>
     )
 }
