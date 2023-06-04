e3aa365602a98b5a56f6cd7c7dc69f4d850c611d
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-04 23:29:52 +0530

    add content to footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,3 +1,33 @@
+import Link from "next/link"
+import { siteConfig } from "../data/siteconfig"
+
 export function SiteFooter() {
-    return <footer></footer>
+    const tabs = siteConfig.tabs.slice()
+    tabs.push(
+        { title: "Team", href: "/team" },
+        { title: "Privacy Policy", href: "/privary-policy" }
+    )
+    return (
+        <footer className="bg-dark-gray px-8 py-12 text-white">
+            <div className="flex justify-center gap-16 md:flex-row">
+                <div>
+                    <h1 className="mb-4 text-lg font-bold">Company</h1>
+                    <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+                        {tabs.map((item) => (
+                            <Link
+                                key={item.href}
+                                href={item.href}
+                                className="text-sm underline-offset-2 hover:underline"
+                            >
+                                {item.title}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                <div className="justify-center md:flex-col">
+                    <h1 className="text-2xl font-extrabold tracking-tighter">ITR Mitra</h1>
+                </div>
+            </div>
+        </footer>
+    )
 }
