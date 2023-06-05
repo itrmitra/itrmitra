7e3251552489620051c3adc5fa9c0ef59807db79
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-05 23:23:58 +0530

    add privacy policy and terms and conditions page

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,10 +1,140 @@
 export default function Pricing() {
     return (
         <>
-            <title>ITR Mitra | Pricing</title>
+            <title>ITR Mitra | Privacy Policy</title>
             <meta name="description" content="" />
-            <div className="grid min-h-screen place-items-center text-3xl font-extrabold">
-                In Development...
+            <div className="px-8 py-12 md:min-h-screen">
+                <h1 className="mb-12 text-4xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
+                    Privacy Policy of ITR Mitra
+                </h1>
+                <p className="rounded-xl border bg-white p-8">
+                    <p className="mb-4">
+                        At ITR Mitra, we are committed to protecting the privacy and security of our
+                        users: personal information. This Privacy Policy outlines how we collect,
+                        use, disclose, and store personal information when you use our ITR (Income
+                        Tax Return) preparation services website. By using our website, you consent
+                        to the practices described in this policy.
+                    </p>
+                    <ul className="space-y-4">
+                        <li>
+                            <b>1. Information we collect:</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <b>a. Personal Information:</b>
+                                    <p>
+                                        We may collect personal information, such as your name,
+                                        email address, contact number, and any other information you
+                                        provide to us when using our services.
+                                    </p>
+                                </div>
+                                <div>
+                                    <b>b. Financial Information:</b>
+                                    <p>
+                                        To facilitate the ITR preparation process, we may collect
+                                        financial information, including income details, deductions,
+                                        and other relevant tax-related data.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>2. Use of information</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <b>a. Personalization:</b>
+                                    <p>
+                                        We may use the information collected to personalize and
+                                        improve your experience with our ITR preparation services.
+                                    </p>
+                                </div>
+                                <div>
+                                    <b>b. ITR Preparation:</b>
+                                    <p>
+                                        Your personal and financial information will be utilized to
+                                        prepare and file your income tax returns accurately.
+                                    </p>
+                                </div>
+                                <div>
+                                    <b>c. Communication:</b>
+                                    <p>
+                                        We may use your contact information to communicate with you
+                                        regarding updates, notifications, and customer support
+                                        related to our services.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>3. Information Sharing:</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <b>a. Service Providers:</b>
+                                    <p>
+                                        We may engage trusted third-party service providers to
+                                        assist us in providing our services, and they may have
+                                        access to your personal information strictly for the
+                                        purposes of providing these services on our behalf.
+                                    </p>
+                                </div>
+                                <div>
+                                    <b>b. Legal Compliance:</b>
+                                    <p>
+                                        We may disclose your personal information if required by
+                                        law, regulation, or a valid legal process.
+                                    </p>
+                                </div>
+                                <div>
+                                    <b>c. Business Transfers:</b>
+                                    <p>
+                                        In the event of a merger, acquisition, or sale of all or a
+                                        portion of our assets, your personal information may be
+                                        transferred as part of the transaction. We will notify you
+                                        of any such change in ownership or control of your personal
+                                        information.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>4. Data Security:</b>
+                            <p>
+                                We take reasonable measures to protect your personal information
+                                from unauthorized access, alteration, or disclosure. However, please
+                                note that no method of transmission over the internet or electronic
+                                storage is entirely secure. We cannot guarantee absolute security of
+                                your data.
+                            </p>
+                        </li>
+                        <li>
+                            <b>5. Third-Party Links:</b>
+                            <p>
+                                Our website may contain links to third-party websites or services.
+                                We are not responsible for the privacy practices or the content of
+                                those websites. We encourage you to review the privacy policies of
+                                any third-party websites you visit.
+                            </p>
+                        </li>
+                        <li>
+                            <b>6. Changes to the Privacy Policy:</b>
+                            <p>
+                                We reserve the right to update or modify this Privacy Policy at any
+                                time. If we make any material changes, we will notify you by posting
+                                the revised policy on our website or by other means of
+                                communication. Your continued use of our services after the changes
+                                indicate your acceptance of the updated Privacy Policy.
+                            </p>
+                        </li>
+                        <li>
+                            <b>7. Contact Us:</b>
+                            <p>
+                                If you have any questions or concerns about this Privacy Policy or
+                                our data practices, please contact us at myitrmitra@gmail.com. It is
+                                essential to regularly review the Privacy Policy to stay informed
+                                about how your personal information is being handled.
+                            </p>
+                        </li>
+                    </ul>
+                </p>
             </div>
         </>
     )
---
+++ src/pages/terms-and-conditions.tsx
@@ -0,0 +1,202 @@
+export default function Pricing() {
+    return (
+        <>
+            <title>ITR Mitra | Terms and Conditions</title>
+            <meta name="description" content="" />
+            <div className="px-8 py-12 md:min-h-screen">
+                <h1 className="mb-12 text-4xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
+                    Terms and Conditions of ITR Mitra
+                </h1>
+                <div className="rounded-xl border bg-white p-8">
+                    <p className="mb-4">
+                        These Terms and Conditions (&quot;Agreement&quot;) govern your use of the
+                        website and services provided by ITR Mitra (&quot;Company,&quot;
+                        &quot;we,&quot; or &quot;us&quot;). By accessing or using our website, you
+                        agree to be bound by these terms and conditions. If you do not agree with
+                        any part of this Agreement, you should not use our website or services.
+                    </p>
+                    <ul className="space-y-4">
+                        <li>
+                            <b>1. Services Provided:</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <p>
+                                        a. Our website provides income tax return (ITR) preparation
+                                        services.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        b. We may offer additional services, such as document
+                                        collection, tax planning advice, and filing assistance. The
+                                        details of these services will be outlined separately on our
+                                        website.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>2. User Responsibilities:</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <p>
+                                        a. You must provide accurate and complete information when
+                                        using our services.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        b. You are responsible for maintaining the confidentiality
+                                        of any login credentials provided to you and for all
+                                        activities that occur under your account.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>3. Service Limitations:</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <p>
+                                        a. While we strive to provide accurate and up-to-date
+                                        information, we do not guarantee the accuracy, completeness,
+                                        or suitability of the content on our website.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        b. Our services are not a substitute for professional
+                                        advice. We recommend consulting with a qualified tax
+                                        professional before making any financial or tax-related
+                                        decisions.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        c. Our opinion has given as per our understanding and
+                                        knowledge of client scenarios. It may vary from person to
+                                        person. Our opinion may be different from other
+                                        professionals’ opinion.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        d. We do not give absolute assurance about our opinion, we
+                                        give reasonable assurance.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>4. Intellectual Property:</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <p>
+                                        a. All content on our website, including text, graphics,
+                                        logos, and images, is protected by copyright and other
+                                        intellectual property laws.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        b. You may not modify, reproduce, distribute, or use any
+                                        content from our website without obtaining prior written
+                                        permission from us.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>5. Third-Party Links:</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <p>
+                                        a. Our website may contain links to third-party websites or
+                                        services that are not owned or controlled by us.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        b. We are not responsible for the content or practices of
+                                        any third-party websites or services. You access them at
+                                        your own risk.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>6. Privacy Policy:</b>
+                            <p>
+                                We collect and process personal information in accordance with our
+                                Privacy Policy. By using our website, you consent to our collection
+                                and use of personal information as outlined in the Privacy Policy.
+                            </p>
+                        </li>
+                        <li>
+                            <b>7. Limitation of Liability:</b>
+                            <p className="space-y-2 pl-4">
+                                <div>
+                                    <p>
+                                        a. To the fullest extent permitted by law, we shall not be
+                                        liable for any direct, indirect, incidental, special, or
+                                        consequential damages arising out of or in connection with
+                                        your use of our website or services.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        b. We do not guarantee the uninterrupted availability of our
+                                        website and reserve the right to modify or discontinue any
+                                        part of our website or services without prior notice.
+                                    </p>
+                                </div>
+                                <div>
+                                    <p>
+                                        c. If any demand received by the Any government authority,
+                                        we are not responsible for any tax liability to be paid by
+                                        us.
+                                    </p>
+                                </div>
+                            </p>
+                        </li>
+                        <li>
+                            <b>8. Indemnification:</b>
+                            <p>
+                                You agree to indemnify and hold us harmless from any claims,
+                                liabilities, damages, losses, or expenses arising out of your use of
+                                our website or services, violation of this Agreement, or
+                                infringement of any intellectual property or other rights of any
+                                third party.
+                            </p>
+                        </li>
+                        <li>
+                            <b>9. Governing Law and Jurisdiction:</b>
+                            <p>
+                                This Agreement shall be governed by and construed in accordance with
+                                the laws of India. Any disputes arising out of or in connection with
+                                this Agreement shall be subject to the exclusive jurisdiction of the
+                                courts located in Parbhani, Maharashtra, India.
+                            </p>
+                        </li>
+                        <li>
+                            <b>10. Modifications:</b>
+                            <p>
+                                We reserve the right to modify these terms and conditions at any
+                                time. Any changes will be effective immediately upon posting on our
+                                website. It is your responsibility to review these terms
+                                periodically.
+                            </p>
+                        </li>
+                    </ul>
+
+                    <p className="mt-4">
+                        By accessing or using our website, you acknowledge that you have read,
+                        understood, and agreed to be bound by these terms and conditions. If you
+                        have any questions or concerns regarding this Agreement, please contact us
+                        at myitrmitra@gmail.com
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
