ed6d451fff39ef7f6eed4af352de972bd8388fb0
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-05 18:38:37 +0530

    add some extra information to ServiceCard

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -10,11 +10,15 @@ export default function Services() {
                 name="description"
                 content="ITR Mitra provides six types of services including Capital Gain Plan, Future & Options Plan and NRI having Foreign Income."
             />
-            <div className="min-h-screen px-8 py-12">
-                <h1 className="mb-8 text-4xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
+            <div className="px-8 py-12 text-center md:min-h-screen md:text-left">
+                <h1 className="mb-4 text-4xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
                     Services
                 </h1>
-                <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+                <p className="mb-12 text-sm md:w-1/2 md:text-base lg:text-lg">
+                    Our team of experts is here to help to save time, money and hassle. Our six
+                    services offer a comprehensive solution for your needs.
+                </p>
+                <div className="grid grid-cols-1 gap-6 text-left md:grid-cols-2 lg:grid-cols-3">
                     {services.map((item) => (
                         <ServiceCard {...item} key={item.name} />
                     ))}
@@ -35,8 +39,9 @@ function ServiceCard({
 }) {
     return (
         <div className="rounded-xl bg-white p-8 shadow-lg">
-            <h1 className="mb-8 text-xl font-bold md:text-2xl">{name}</h1>
+            <h1 className="mb-4 text-xl font-bold md:text-2xl">{name}</h1>
             <div className="space-y-2">
+                <p className="text-medium mb-4 text-sm md:text-base">Suited For:</p>
                 {features.map((item, idx) => (
                     <div className="flex items-start gap-2 text-sm md:text-base">
                         <div className="min-w-8 h-8">
