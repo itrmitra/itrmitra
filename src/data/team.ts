e41cada3887b49c4ce34afc493502953c0fc5e02
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-05 18:28:23 +0530

    add teams page

---
+++ src/data/team.ts
@@ -0,0 +1,32 @@
+export const team = [
+    {
+        name: "Jitendra Joshi",
+        intro: "Introducing Jitendra, the Founder of ITR Mitra, a niche service specializing in income tax, business setups, and TDS. With 5 years of experience and a comprehensive understanding of tax regulations, Jitendra helps clients navigate compliance and avoid consequences. His expertise, listening ability, and communication skills make him a trusted professional who delivers tailored tax solutions. Maximize your financial potential and ensure compliance with Jitendra, the Founder of ITR Mitra.",
+        imageUrl: "/jitendra-joshi.jpg",
+    },
+    {
+        name: "Bhushan Patil",
+        intro: "Bhushan Patil is a finance professional with 5 years of experience in income tax, GST, audit, and transfer pricing. He holds a master’s degree in finance from Pune University and delivers exceptional results with strong analytical skills and attention to detail. Bhushan provides valuable insights and strategic solutions, staying updated with financial regulations and industry trends. He is known for effective communication and a collaborative approach in complex financial landscapes.",
+        imageUrl: "/bhushan-patil.jpg",
+    },
+    {
+        name: "Niraj Mane",
+        intro: "Niraj Mane is an experienced professional with expertise in internal control, direct and indirect taxation, and project financing matters. With a strong financial and accounting background, he excels in understanding the business environment. Niraj has successfully completed assignments for renowned companies, showcasing his proficiency in audit and assurance services, GST services, taxation, and regulatory compliance services.",
+        imageUrl: "/niraj-mane.jpg",
+    },
+    {
+        name: "CA Yashvant Mehta",
+        intro: "CA Yashvant Mehta is an Associate Member of Institute of Chartered Accountants of India since 2018. He is also trying to get his way into the Information Systems Audit. Yashvant is an excellent negotiator for handling and resolving critical blockers for the Client. His expertise is Crypto, Future and options and NRI. He is a reliable advisor on Financial Investments and Income Tax planning. Yashvant has done multiple assessments in Income Tax and GST.",
+        imageUrl: "yashvant-mehta.jpg",
+    },
+    {
+        name: "CA Kavya Thama",
+        intro: "CA Kavya Thama has over 3yrs of article training experience in the field of direct taxes, indirect taxes, statutory audits, tax audits. She has worked with many clients from different industries including banking, telecom, consumer goods, pharmaceutical and software industry. Apart from holding a Chartered accountancy degree she has completed her Company secretary (Executive program) and completed her postgraduate from Pune university.",
+        imageUrl: "kavya-thama.jpg",
+    },
+    {
+        name: "CA Omkar Lambe",
+        intro: "CA Omkar Lambe is an Associate Member of Institute of Chartered Accountants of India since 2021. He is associated with ITR Mitra for Consultation and notices parts. He has completed his article training from a well known CA firm based in Pune. Within a short period of time Omkar has represented many Income Tax cases, GST Notices. He is expert in the field of solving Queries raised by Government officials. Notice section is his core competency.",
+        imageUrl: "omkar-lambe.jpg",
+    },
+]
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,11 +1,47 @@
+import { team } from "../data/team"
+
 export default function Team() {
     return (
         <>
-            <title>ITR Mitra | Contact</title>
-            <meta name="description" content="" />
-            <div className="grid min-h-screen place-items-center text-3xl font-extrabold">
-                In Development...
+            <title>ITR Mitra | Team</title>
+            <meta
+                name="description"
+                content="Our team's creativity, innovation, and drive have helped us to transform
+                    individual ideas into a functional and successful organization."
+            />
+            <div className="px-8 py-12 text-center md:min-h-screen md:text-left">
+                <h1 className=" mb-4 text-4xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
+                    Meet Our Team
+                </h1>
+                <p className="mb-12 text-sm md:w-1/2 md:text-base lg:text-lg">
+                    Our team's creativity, innovation, and drive have helped us to transform
+                    individual ideas into a functional and successful organization.
+                </p>
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                    {team.map((item) => (
+                        <InfoCard key={item.name} {...item} />
+                    ))}
+                </div>
             </div>
         </>
+    )
+}
+
+function InfoCard({ name, intro, imageUrl }: { name: string; intro: string; imageUrl: string }) {
+    return (
+        <div className="flex flex-col overflow-hidden rounded-xl bg-white p-8 shadow-lg lg:flex-row lg:items-start">
+            <div className="mx-auto h-36 w-36 lg:h-full lg:w-56">
+                <img
+                    src={imageUrl}
+                    className="mx-auto h-36 w-36 rounded-full object-cover lg:h-full lg:w-56 lg:rounded-lg"
+                />
+            </div>
+            <div className="flex-1 pt-6 text-center lg:pl-8 lg:pt-0 lg:text-left">
+                <h1 className="mb-2 text-xl font-semibold text-brand md:text-2xl lg:text-3xl">
+                    {name}
+                </h1>
+                <p className="text-sm md:text-base lg:text-lg">{intro}</p>
+            </div>
+        </div>
     )
 }
