e4e88ee3466b67edcef686fd3c8dd1691f7d959a
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-06 15:15:52 +0530

    fix imageurls

--- src/data/aboutus.ts
+++ src/data/aboutus.ts
@@ -3,35 +3,35 @@ export const aboutus = [
         title: "Expertise That Empowers",
         content:
             "Our seasoned professionals have mastered the intricacies of taxation, ensuring accurate and reliable solutions tailored to your needs.",
-        imageUrl: "/empower.svg",
+        imageUrl: "/aboutus/empower.svg",
         alt: "Empower",
     },
     {
         title: "Seamless and Swift",
         content:
             "Say goodbye to tax stress! Our user-friendly online platform ensures a smooth experience from start to finish.",
-        imageUrl: "/mobile-user.svg",
+        imageUrl: "/aboutus/mobile-user.svg",
         alt: "Girl looking at her phone",
     },
     {
         title: "Personalized Guidance",
         content:
             "We understand that your tax situation is unique. Count on us to provide tailored advice and proactive tax planning strategies.",
-        imageUrl: "/guidance.svg",
+        imageUrl: "/aboutus/guidance.svg",
         alt: "Personalized Guidance",
     },
     {
         title: "Trustworthy Partnership",
         content:
             "Trust is our foundation. Rest assured that your confidential information is secure with us, and our integrity is unwavering.",
-        imageUrl: "/partnership.svg",
+        imageUrl: "/aboutus/partnership.svg",
         alt: "Agreement between two persons",
     },
     {
         title: "Unlock Your Financial Potential",
         content:
             "Let us help you navigate the tax landscape, optimizing your tax savings and empowering your financial goals.",
-        imageUrl: "/growth.svg",
+        imageUrl: "/aboutus/growth.svg",
         alt: "Person climbing steps",
     },
 ]
--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -3,19 +3,19 @@ export const testimonials = [
         name: "Sanit Jain",
         text: "Very satisfied with ITR Mitra service. Mr. Jitendra very professional in his behavior and filed my return on the same day without any hassle. I got all my queries answered before filing the returns and had very quick response from him.I really liked the service and excellent work. Thanks ITR Mitra",
         numStars: 4,
-        imageUrl: "/sanit-jain.jpg",
+        imageUrl: "/testimonials/sanit-jain.jpg",
     },
     {
         name: "Vikas Joshi",
         text: "Very Professionalism, Responsiveness. Jitendra was very responsive and prompt throughout the return filing process. He helped me to fill returns with the friendly behaviour.",
         numStars: 5,
-        imageUrl: "/vikas-joshi.jpg",
+        imageUrl: "/testimonials/vikas-joshi.jpg",
     },
     {
         name: "Sameer Tiwari",
         text: "I have been taking tax consulting service from Jitendra for past 3 plus years. He has given me excellent guidance and always prompt on filing my return taxes. I would recommend his service to my friends too. Thanks for your help and guidance thus far. Keep up the good work.",
         numStars: 5,
-        imageUrl: "/sameer-tiwari.jpg",
+        imageUrl: "/testimonials/sameer-tiwari.jpg",
     },
     {
         name: "Nilam Dubey",
@@ -26,12 +26,12 @@ export const testimonials = [
         name: "Shrutika Wani",
         text: "Very satisfied with ITR Mitra service. Mr Jitendra is very professional in his behavior and filed my return on the same day without any hassle. I got all my queries answered before filing the returns and had very quick response from him.I really liked the service and excellent work. Thanks ITR Mitra.",
         numStars: 5,
-        imageUrl: "/shrutika-wani.jpg",
+        imageUrl: "/testimonials/shrutika-wani.jpg",
     },
     {
         name: "Brijesh Nair",
         text: "I'm filing my ITR from Jitendra from last 3 years, he is very professional in his work, excellent command over taxation and give me best advice as possible, guided me soo much with my taxes. Now I can focus on my business and need not to worry about tax and accounts part, Jitendra take care of everything. Thank you Jitendra, keep it up ITR Mitra.",
         numStars: 4,
-        imageUrl: "/brijesh-nair.jpg",
+        imageUrl: "/testimonials/brijesh-nair.jpg",
     },
 ]
