db266a93357bacaea2cd6ba701f9f11bce0082e8
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-12 23:57:46 +0530

    fix: set sentence case to title of benefit card title

--- src/data/benefits.ts
+++ src/data/benefits.ts
@@ -1,28 +1,28 @@
 export const benefits = [
     {
-        title: "Avoid late fees and penalties",
+        title: "Avoid Late Fees and Penalties",
         content: "Avoid a maximum penanlty of ₹10,000 if your annual income exceeds ₹5L",
     },
     {
-        title: "Avoid tax notices",
+        title: "Avoid Tax Notices",
         content: "Taxpayers are often served notices from the IT department for delayed returns",
     },
     {
-        title: "Build financial documentation",
+        title: "Build Financial Documentation",
         content:
             "Tax returns are proof of your financial investment and useful for loans and visas",
     },
     {
-        title: "Carry forward your losses",
+        title: "Carry Forward Your Losses",
         content: "You can carry forward losses against house property and depreciation",
     },
     {
-        title: "Claim tax refunds",
+        title: "Claim Tax Refunds",
         content:
             "You can claim tax refund, the earlier you file the sooner you will get the refund",
     },
     {
-        title: "Quick visa processing",
+        title: "Quick Visa Processing",
         content:
             "Most embassies and consulates require you to submit IT returns for the last 3 years",
     },
