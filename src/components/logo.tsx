2e6a018b017ec045d0ad81e5b8c216ce4092b787
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-12 23:50:58 +0530

    increase logo text size

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export function Logo() {
     return (
         <Link href="/" className="flex flex-col items-center justify-center">
-            <h1 className="text-2xl font-extrabold tracking-tighter">ITR Mitra</h1>
+            <h1 className="text-3xl font-extrabold tracking-tighter">ITR Mitra</h1>
             <p className="text-xs italic">Your ITR, Our Expertise</p>
         </Link>
     )
