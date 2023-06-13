0903b5d06c68b17ed4b2c9864db8808bfeccd342
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-13 15:29:15 +0530

    use headingVariants

--- src/pages/useful-links.tsx
+++ src/pages/useful-links.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head"
 import { usefulLinks } from "../data/useful-links"
+import { headingVariants } from "../components/ui/heading"
+import { cn } from "../lib/utils"
 
 export default function UsefulLinks() {
     return (
@@ -11,11 +13,14 @@ export default function UsefulLinks() {
                     content="ITR Mitra helps in filing Income Tax Returns and provides expert assistance with the whole ITR filing process. We also help custombers with Income Tax Notices and further procedures."
                 />
             </Head>
-            <div className="flex flex-col items-center justify-center px-8 py-12 md:min-h-screen">
-                <h1 className="mb-4 text-4xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
-                    Useful Links
-                </h1>
-                <p className="mb-8 text-center text-sm md:text-base lg:text-lg">
+            <div className="flex flex-col items-stretch justify-center px-8 py-12 md:min-h-screen">
+                <h1 className={cn(headingVariants(), "mx-auto mb-4")}>Useful Links</h1>
+                <p
+                    className={cn(
+                        headingVariants({ variant: "subtext", size: "subtext" }),
+                        "mx-auto mb-12"
+                    )}
+                >
                     These links are very helpful in many tax returns scenarios. We thought this
                     might be helpful for some of our users.
                 </p>
@@ -24,7 +29,7 @@ export default function UsefulLinks() {
                         <div className="">
                             <a
                                 href={item.href}
-                                className="inline-block w-full cursor-pointer rounded-xl border bg-white p-8 text-center text-sm transition-all hover:bg-brand hover:text-white hover:underline hover:shadow-lg md:text-base lg:text-base"
+                                className="inline-block w-full cursor-pointer rounded-xl border bg-white p-8 text-center  transition-all hover:bg-brand hover:text-white hover:underline hover:shadow-lg  lg:text-lg"
                             >
                                 {item.title}
                             </a>
