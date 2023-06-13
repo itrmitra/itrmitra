b560c82c10d0b9bed8921782e123b52b95355bac
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-13 14:57:28 +0530

    fix margin under subtext

--- src/components/info-sections/landing-section.tsx
+++ src/components/info-sections/landing-section.tsx
@@ -7,10 +7,15 @@ export default function LandingSection() {
         <div className="flex flex-col-reverse items-center px-8 py-12 md:min-h-screen md:flex-row">
             <div className="flex-1">
                 <div className="mx-auto flex w-3/4 flex-col items-center text-center md:items-start md:text-start">
-                    <h1 className={headingVariants({})}>
+                    <h1 className={cn(headingVariants(), "mb-4")}>
                         Simplify Your Tax Filing Experience with Expert Guidance
                     </h1>
-                    <p className={cn(headingVariants({ variant: "subtext", size: "subtext" }))}>
+                    <p
+                        className={cn(
+                            headingVariants({ variant: "subtext", size: "subtext" }),
+                            "mb-8"
+                        )}
+                    >
                         Maximize Returns, Minimize Stress: Your Trusted Tax Filing Partner
                     </p>
                     <WhatsappButton />
