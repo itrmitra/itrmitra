0a0e97a2ba7892a03c8e91b749c59d734a60f1f6
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-13 15:59:47 +0530

    add schema data structure to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,28 @@ import ItrFilingProcessSection from "../components/info-sections/itr-filiing-pro
 import LandingSection from "../components/info-sections/landing-section"
 import OfferSection from "../components/info-sections/offer-section"
 import TestimonialsSection from "../components/info-sections/testimonials-section"
+import Script from "next/script"
+
+const content = {
+    "@context": "https://schema.org",
+    "@type": "FinancialService",
+    name: "ITR Mitra",
+    alternateName: "ITR Mitra",
+    url: "https://itrmitra.in/",
+    telephone: "+91-9359412287",
+    address: {
+        "@type": "PostalAddress",
+        streetAddress: "Office No. 5, Building No.2, New Laxmi Narayan Park, Koregaon Park",
+        addressLocality: "Pune",
+        postalCode: "411001",
+        addressCountry: "IN",
+    },
+    contactPoint: {
+        "@type": "ContactPoint",
+        telephone: "+91-9359412287",
+        contactType: "customer service",
+    },
+}
 
 export default function Home() {
     return (
@@ -17,6 +39,13 @@ export default function Home() {
                     title="ITR Mitra | Your Personal Tax Filing Expert"
                     name="description"
                     content="ITR Mitra helps in filing Income Tax Returns and provides expert assistance with the whole ITR filing process. We also help custombers with Income Tax Notices and further procedures."
+                />
+
+                <Script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify(content, null, "\t"),
+                    }}
                 />
             </Head>
             <OfferSection />
