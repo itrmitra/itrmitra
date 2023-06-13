2aefb45de548c98fcb152bfdfa2f5a6e1497158f
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-06-13 13:57:41 +0530

    cleanup tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,26 +13,7 @@ module.exports = {
             },
         },
         extend: {
-            scale: {
-                "-100": "-1",
-            },
             keyframes: {
-                slideDownAndFade: {
-                    from: { opacity: 0, transform: "translateY(-2px)" },
-                    to: { opacity: 1, transform: "translateY(0)" },
-                },
-                slideLeftAndFade: {
-                    from: { opacity: 0, transform: "translateX(2px)" },
-                    to: { opacity: 1, transform: "translateX(0)" },
-                },
-                slideUpAndFade: {
-                    from: { opacity: 0, transform: "translateY(2px)" },
-                    to: { opacity: 1, transform: "translateY(0)" },
-                },
-                slideRightAndFade: {
-                    from: { opacity: 0, transform: "translateX(-2px)" },
-                    to: { opacity: 1, transform: "translateX(0)" },
-                },
                 "accordion-down": {
                     from: { height: 0 },
                     to: { height: "var(--radix-accordion-content-height)" },
@@ -43,37 +24,10 @@ module.exports = {
                 },
             },
             animation: {
-                slideDownAndFade: "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-                slideLeftAndFade: "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-                slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-                slideRightAndFade: "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
             },
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
-            backgroundColor: {
-                brand: "var(--brand-color)",
-                "light-brand": "var(--light-brand-color)",
-                "dark-gray": "var(--dark-gray)",
-            },
-            borderColor: {
-                brand: "var(--brand-color)",
-                "dark-gray": "var(--dark-gray)",
-            },
-            textColor: {
-                primary: "var(--dark-gray)",
-                brand: "var(--brand-color)",
-            },
-            fill: {
-                brand: "var(--brand-color)",
-                "light-brand": "var(--light-brand-color)",
-                "dark-gray": "var(--dark-gray)",
-            },
-            stroke: {
+            colors: {
                 brand: "var(--brand-color)",
                 "light-brand": "var(--light-brand-color)",
                 "dark-gray": "var(--dark-gray)",
