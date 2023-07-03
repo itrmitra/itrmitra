1021dccf7363383d33a2841b7b4095932d2623e2
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-07-03 14:47:39 +0530

    remove redundant Head tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,30 +12,7 @@ import { siteConfig } from "../data/siteconfig"
 export default function Home() {
     return (
         <>
-
-            <Head>
-                <title>ITR Mitra | Home</title>
-                <meta
-                    title="ITR Mitra | Your Personal Tax Filing Expert"
-                    name="description"
-                    content="ITR Mitra helps in filing Income Tax Returns and provides expert assistance with the whole ITR filing process. We also help custombers with Income Tax Notices and further procedures."
-                />
-                
-                <meta
-                    title="ITR Filing | Your Personal Tax Filing Expert"
-                    name="description"
-                    content="ITR Mitra helps in filing Income Tax Returns and provides expert assistance with the whole ITR filing process. We also help custombers with Income Tax Notices and further procedures."
-                />
-                <Script
-                    type="application/ld+json"
-                    dangerouslySetInnerHTML={{
-                        __html: JSON.stringify(content, null, "\t"),
-                    }}
-                />
-            </Head>
-
             <CustomHead {...siteConfig.pageInfo.home} />
-
             <OfferSection />
             <LandingSection />
             <FormFilingSection />
