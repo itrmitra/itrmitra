46912e7db6ff65fa933833284f472a6f5faf6cb1
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-07-03 15:20:27 +0530

    increase icon size

--- src/components/FloatingContactButtons.tsx
+++ src/components/FloatingContactButtons.tsx
@@ -5,7 +5,7 @@ import { buttonVariants } from "./ui/button"
 
 export default function FloatingContactButtons() {
     return (
-        <div className="fixed bottom-0 right-0 z-50 mb-4 mr-4 flex flex-col items-center gap-4">
+        <div className="fixed bottom-0 right-0 z-50 mb-4 mr-4 flex flex-col items-center gap-4 md:hidden">
             <a
                 href={`https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP_NUMBER}`}
                 role="button"
@@ -13,19 +13,19 @@ export default function FloatingContactButtons() {
                 target="_blank"
                 className={cn(
                     buttonVariants({ variant: "filled" }),
-                    "text-primary z-0 h-10 w-10 gap-2 bg-white p-2 shadow-lg"
+                    "text-primary z-0 h-12 w-12 gap-2 bg-white p-2 shadow-lg"
                 )}
             >
-                <img src="/whatsapp.svg" className="h-7 w-7" alt="Whatsapp Logo" />
+                <img src="/whatsapp.svg" className="h-8 w-8" alt="Whatsapp Logo" />
             </a>
             <Link
                 href="/#form-filing-section"
                 className={cn(
                     buttonVariants({ variant: "filled" }),
-                    "z-0 h-10 w-10 gap-2 bg-white p-2 shadow-lg"
+                    "z-0 h-12 w-12 gap-2 bg-brand p-2 shadow-lg"
                 )}
             >
-                <Mail className="fill-brand stroke-white" />
+                <Mail className="fill-white stroke-brand" />
             </Link>
         </div>
     )
