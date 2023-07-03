e0e1750f612721b55bef8f6a6c9971b4c386143e
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-07-03 15:05:03 +0530

    add Floating Contact Buttons to body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { Head, Html, Main, NextScript } from "next/document"
+import FloatingContactButtons from "../components/FloatingContactButtons"
 
 export default function Document() {
     return (
@@ -7,6 +8,7 @@ export default function Document() {
             <body className="min-h-screen bg-gray-100 text-dark-gray">
                 <Main />
                 <NextScript />
+                <FloatingContactButtons />
             </body>
         </Html>
     )
