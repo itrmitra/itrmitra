22b3f0259a662f661ea9022472f14a579fa03830
itrmitra <134064715+itrmitra@users.noreply.github.com>
2023-08-18 15:23:41 +0530

    Update services.ts

--- src/data/services.ts
+++ src/data/services.ts
@@ -2,26 +2,26 @@ export const services = [
     {
         highlight: true,
         name: "Salary & House Property Plan",
-        priceBefore: "1599",
-        priceAfter: "799",
+        priceBefore: "1999",
+        priceAfter: "999",
         usefulFor: [
             "Single & Multiple Employers",
             "Single & Multiple House Property",
             "Income from Other Sources",
         ],
-        extraInfo: ["Terms and Conditions Apply", "Offer valid until 31st July, 2023"],
+        extraInfo: ["Terms and Conditions Apply", "Offer valid"],
     },
     {
         name: "Capital Gain Plan",
         priceBefore: "3499",
-        priceAfter: "1499",
+        priceAfter: "1999",
         usefulFor: ["Capital Gain Income from Sale of Property, Mutual Funds, Shares, etc."],
         includes: ["Salary & House Property Plan"],
     },
     {
         name: "Business & Professional Plan",
         priceBefore: "4499",
-        priceAfter: "2499",
+        priceAfter: "2999",
         usefulFor: [
             "Business & Professional Income (Non Audit) *",
             "Speculative Income / Intraday Trading of Shares",
@@ -36,7 +36,7 @@ export const services = [
     {
         name: "Future & Options Plan",
         priceBefore: "4999",
-        priceAfter: "2999",
+        priceAfter: "3499",
         usefulFor: ["F&O Income/Loss (Non Audit)"],
         includes: [
             "Salary & House Property Plan",
