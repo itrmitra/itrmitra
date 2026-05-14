9120f470534f2434f11ba8f5765310ce1f3a3a1a
itrmitra <134064715+itrmitra@users.noreply.github.com>
2026-05-14 21:58:10 +0530

    Update services.tsx

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -34,8 +34,9 @@ export default function Services() {
                 <div className="mt-6 flex justify-center">
     <div className="flex w-full max-w-md flex-col items-center justify-center gap-8 rounded-xl bg-green-500 p-8 text-center text-white shadow-lg">
         <h1 className="mb-4 text-3xl font-bold tracking-tight lg:text-4xl">
-             Got 10+ returns? <br />
-            You qualify for our Premium Volume Rate. <br />
+             Have 10+ ITRs..? <br />
+            You're qualifing for our bulk discount.. <br />
+            <br/>
             Let’s talk..!
         </h1>
 
